cd8bcb45b6fb98066b5efc74f9e9a9155edd1f12
xiaozemarry <562113226@qq.com>
2017-03-14 17:03:09 +0800

    Update 启动.bat

--- src/启动.bat
+++ src/启动.bat
@@ -8,3 +8,20 @@ cd %CURRENT_DIR%
 set TITLE=WEBSERVER-PLATFORM-V1 sinopec 8080
 set JAVA_OPTS=-Xms1024m -Xmx1024m -XX:PermSize=64M -XX:MaxPermSize=128m
 bin\startup.bat
+
+@echo off
+set "CURRENT_DIR=%cd%"
+set "JAVA_OPTS=-Xms1024m -Xmx1024m"
+cd ..
+set "JAVA_HOME=jdk1.7.0_80"
+set "JRE_HOME=jre7"
+cd %CURRENT_DIR%
+
+set TITLE=tomcat2 8082
+set "JAVA_OPTS=-Xms256m -Xmx1024m -verbose:gc -Xloggc:s:/gc.log -XX:+PrintGCDetails"
+
+set JPDA_ADDRESS=2366
+set JPDA_SUSPEND=n
+set JPDA_TRANSPORT=dt_socket
+set JPDA_server=y
+bin\startup.bat
